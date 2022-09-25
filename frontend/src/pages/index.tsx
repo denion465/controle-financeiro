6b41bc6ec464f67ac55798e094d1957862289a94
Daniel Vidal <danielsv465@hotmail.com>
2022-09-25 20:46:23 -0300

    Initial commit

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>
+        Hello world!
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
